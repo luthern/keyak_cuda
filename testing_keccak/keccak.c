808fbcfcc1eb8697735076d6fd11aff1c29787d3
Conor <conorpp@vt.edu>
2016-04-20 20:16:49 -0400

    rename keccak_testing

---
+++ testing_keccak/keccak.c
@@ -0,0 +1,179 @@
+#include "keccak.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "misc.h"
+#include <string.h>
+#include <unistd.h>
+#define     UNROLL_CHILOOP
+
+
+#if defined(__GNUC__)
+#define ALIGN __attribute__ ((aligned(32)))
+#elif defined(_MSC_VER)
+#define ALIGN __declspec(align(32))
+#else
+#define ALIGN
+#endif
+
+#if defined(_MSC_VER)
+#define ROL64(a, offset) _rotl64(a, offset)
+#elif defined(UseSHLD)
+    #define ROL64(x,N) ({ \
+    register UINT64 __out; \
+    register UINT64 __in = x; \
+    __asm__ ("shld %2,%0,%0" : "=r"(__out) : "0"(__in), "i"(N)); \
+    __out; \
+    })
+#else
+#define ROL64(a, offset) ((((UINT64)a) << offset) ^ (((UINT64)a) >> (64-offset)))
+#endif
+
+const UINT8 KeccakF_RotationConstants[25] =
+{
+     1,  3,  6, 10, 15, 21, 28, 36, 45, 55,  2, 14, 27, 41, 56,  8, 25, 43, 62, 18, 39, 61, 20, 44
+};
+
+const UINT8 KeccakF_PiLane[25] =
+{
+    10,  7, 11, 17, 18,  3,  5, 16,  8, 21, 24,  4, 15, 23, 19, 13, 12,  2, 20, 14, 22,  9,  6,  1
+};
+
+#if    defined(DIVISION_INSTRUCTION)
+#define    MOD5(argValue)    ((argValue) % 5)
+#else
+const UINT8 KeccakF_Mod5[10] =
+{
+    0, 1, 2, 3, 4, 0, 1, 2, 3, 4
+};
+#define    MOD5(argValue)    KeccakF_Mod5[argValue]
+#endif
+
+static tKeccakLane KeccakF1600_GetNextRoundConstant( UINT8 *LFSR )
+{
+    tSmallUInt i;
+    tKeccakLane    roundConstant;
+    tSmallUInt doXOR;
+    tSmallUInt tempLSFR;
+
+    roundConstant = 0;
+    tempLSFR = *LFSR;
+    for(i=1; i<128; i <<= 1)
+    {
+        doXOR = tempLSFR & 1;
+        if ((tempLSFR & 0x80) != 0)
+            // Primitive polynomial over GF(2): x^8+x^6+x^5+x^4+1
+            tempLSFR = (tempLSFR << 1) ^ 0x71;
+        else
+            tempLSFR <<= 1;
+
+        if ( doXOR != 0 )
+            roundConstant ^= (tKeccakLane)1ULL << (i - 1);
+    }
+    *LFSR = (UINT8)tempLSFR;
+    return ( roundConstant );
+}
+
+void KeccakP1600_StatePermute(void *argState, UINT8 rounds, UINT8 LFSRinitialState)
+{
+    tSmallUInt x, y, round;
+    tKeccakLane        temp;
+    tKeccakLane        BC[5];
+    tKeccakLane     *state;
+    UINT8           LFSRstate;
+
+    state = (tKeccakLane*)argState;
+    LFSRstate = LFSRinitialState;
+    round = rounds;
+    do
+    {
+        // Theta
+        for ( x = 0; x < 5; ++x )
+        {
+            BC[x] = state[x] ^ state[5 + x] ^ state[10 + x] ^ state[15 + x] ^ state[20 + x];
+        }
+        for ( x = 0; x < 5; ++x )
+        {
+            temp = BC[MOD5(x+4)] ^ ROL64(BC[MOD5(x+1)], 1);
+            for ( y = 0; y < 25; y += 5 )
+            {
+                state[y + x] ^= temp;
+            }
+        }
+
+        // Rho Pi
+        temp = state[1];
+        for ( x = 0; x < 24; ++x )
+        {
+            BC[0] = state[KeccakF_PiLane[x]];
+            state[KeccakF_PiLane[x]] = ROL64( temp, KeccakF_RotationConstants[x] );
+            temp = BC[0];
+        }
+
+        //    Chi
+        for ( y = 0; y < 25; y += 5 )
+        {
+#if defined(UNROLL_CHILOOP)
+            BC[0] = state[y + 0];
+            BC[1] = state[y + 1];
+            BC[2] = state[y + 2];
+            BC[3] = state[y + 3];
+            BC[4] = state[y + 4];
+#else
+            for ( x = 0; x < 5; ++x )
+            {
+                BC[x] = state[y + x];
+            }
+#endif
+            for ( x = 0; x < 5; ++x )
+            {
+                state[y + x] = BC[x] ^((~BC[MOD5(x+1)]) & BC[MOD5(x+2)]);
+            }
+        }
+
+        //    Iota
+        state[0] ^= KeccakF1600_GetNextRoundConstant(&LFSRstate);
+    }
+    while( --round != 0 );
+}
+
+/*int main()*/
+/*{*/
+    /***/
+    /*UINT8 st = 0xd5;*/
+    /*UINT8 rounds = 12;*/
+    /*UINT8 j;*/
+    /*tKeccakLane op;*/
+    /*for (j = 0; j < rounds; j++)*/
+    /*{*/
+        /*op = KeccakF1600_GetNextRoundConstant(&st);*/
+        /*printf("Round %d value %llX\n", j+1, op);*/
+    /*}*/
+
+    /**/
+    /*char * input = malloc(200);*/
+    /*FILE *f = fopen("input", "rb");*/
+    /*fread(input, 200, 1, f); */
+    /*fclose(f);*/
+    /*int i;*/
+    /*for (i = 0; i < 200; i++)*/
+    /*{*/
+        /*printf("%02hhx", input[i]);*/
+    /*}*/
+    /*printf("\n\n");*/
+    /*struct timer t;*/
+    /*memset(&t, 0, sizeof(struct timer));*/
+    /*timer_start(&t, "1000000 sessions");*/
+    /*for (i = 0; i < 1000000; i++)*/
+    /*{*/
+        /*PERMUTE(input);*/
+    /*}*/
+    /*timer_end(&t);*/
+    /*for (i = 0; i < 200; i++)*/
+    /*{*/
+        /*printf("%02hhx", input[i]);*/
+    /*}*/
+    /*printf("\n\n");*/
+    /*return 0;*/
+/*}*/
+
+
