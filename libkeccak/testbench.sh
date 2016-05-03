4bc4e7ded431bf9078f4eb7b8b9c5c583893ca15
Noah Luther <nluther@vt.edu>
2016-05-03 00:38:57 -0400

    updated testbench for class demo

--- libkeccak/testbench.sh
+++ libkeccak/testbench.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-i=10
+i=10000
 echo "Running for $i iterations"
-python -c "print 'A'*4000000" > input.txt
+python -c "print 'A'*1600" > input.txt
 ./keyak AAAAAAAAAAAAAAAAAAAAAAAAAAAAA input.txt out.data -m ABEF14230DFE -n abef912 -i $i
